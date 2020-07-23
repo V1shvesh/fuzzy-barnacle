d7a73b3f5659bab76fc5755bb23ae6ce3f3869cf
V1shvesh <vishvesh.ext@grofers.com>
2020-07-24 00:55:23 +0530

    Add codepush specific checks

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const core = require('@actions/core');
 const { context } = require('@actions/github');
 
+const LABEL_CODEPUSH = 'codepush';
+const LABEL_QA_PASSED = 'QA passed';
+
 try {
     const PRMeta = context.payload.pull_request;
-
+    const baseRef = PRMeta.base.ref;
+    const labels = PRMeta.labels.map(label => label.name);
     console.log(PRMeta);
+    
+    switch (context.action) {
+        case 'opened':
+        case 'edited': {
+
+            const isCodePush = labels.includes(LABEL_CODEPUSH);
+
+            if (isCodePush) {
+                performCodePushChecks(baseRef, labels);
+            }
+            break;
+        }
+        case 'labeled':
+        case 'unlabeled': {
+            const label = context.payload.label.name;
+            if ([LABEL_CODEPUSH, LABEL_QA_PASSED].includes(label)) {
+                performCodePushChecks(baseRef, labels);
+            }
+            break;
+        }
+        default:
+            break;
+    }
 } catch (error) {
     core.setFailed(error.message);
+}
+
+function performCodePushChecks(baseRef, labels) {
+    if (baseRef !== 'release') {
+        throw new Error('Please set the base branch to release');
+    }
+    else if (!labels.includes(LABEL_QA_PASSED)) {
+        throw new Error('Please get QA approval');
+    }
 }
